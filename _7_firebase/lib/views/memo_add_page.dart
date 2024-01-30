fc75f8cd4a18d6bb6ac199c46dc0ff85e18eaeb9
jh4843 <creamboy1@naver.com>
2024-01-31 00:23:18 +0900

    update
    update

--- _7_firebase/lib/views/memo_add_page.dart
+++ _7_firebase/lib/views/memo_add_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
 import '../models/memo.dart';
 
 class MemoAddPage extends StatefulWidget {
@@ -68,101 +69,4 @@ class _MemoAddPage extends State<MemoAddPage> {
       ),
     );
   }
-
-  // InterstitialAd? _interstitialAd;
-  // int _numInterstitialLoadAttempts = 0;
-  // int maxFailedLoadAttempts = 3;
-
-  // void _createInterstitialAd() {
-  //   InterstitialAd.load(
-  //       adUnitId: InterstitialAd.testAdUnitId,
-  //       request: request,
-  //       adLoadCallback: InterstitialAdLoadCallback(
-  //         onAdLoaded: (InterstitialAd ad) {
-  //           print('$ad loaded');
-  //           _interstitialAd = ad;
-  //           _numInterstitialLoadAttempts = 0;
-  //         },
-  //         onAdFailedToLoad: (LoadAdError error) {
-  //           print('InterstitialAd failed to load: $error.');
-  //           _numInterstitialLoadAttempts += 1;
-  //           _interstitialAd = null;
-  //           if (_numInterstitialLoadAttempts <= maxFailedLoadAttempts) {
-  //             _createInterstitialAd();
-  //           }
-  //         },
-  //       ));
-
-  // }
-
-  // void _showInterstitialAd() {
-  //   if (_interstitialAd == null) {
-  //     print('Warning: attempt to show interstitial before loaded.');
-  //     return;
-  //   }
-  //   _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
-  //     onAdShowedFullScreenContent: (InterstitialAd ad) =>
-  //         print('ad onAdShowedFullScreenContent.'),
-  //     onAdDismissedFullScreenContent: (InterstitialAd ad) {
-  //       print('$ad onAdDismissedFullScreenContent.');
-  //       ad.dispose();
-  //       _createInterstitialAd();
-  //     },
-  //     onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
-  //       print('$ad onAdFailedToShowFullScreenContent: $error');
-  //       ad.dispose();
-  //       _createInterstitialAd();
-  //     },
-  //   );
-  //   _interstitialAd!.show();
-  //   _interstitialAd = null;
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text('메모 추가'),
-  //     ),
-  //     body: Container(
-  //       padding: EdgeInsets.all(20),
-  //       child: Center(
-  //         child: Column(
-  //           children: <Widget>[
-  //             TextField(
-  //               controller: titleController,
-  //               decoration: InputDecoration(
-  //                   labelText: '제목', fillColor: Colors.blueAccent),
-  //             ),
-  //             Expanded(
-  //                 child: TextField(
-  //                   controller: contentController,
-  //                   keyboardType: TextInputType.multiline,
-  //                   maxLines: 100,
-  //                   decoration: InputDecoration(labelText: '내용'),
-  //                 )),
-  //             MaterialButton(
-  //               onPressed: () {
-  //                 widget.reference
-  //                     .push()
-  //                     .set(Memo(
-  //                     titleController!.value.text,
-  //                     contentController!.value.text,
-  //                     DateTime.now().toIso8601String())
-  //                     .toJson())
-  //                     .then((_) {
-  //                   Navigator.of(context).pop();
-  //                 });
-  //                 _showInterstitialAd();
-  //               },
-  //               child: Text('저장하기'),
-  //               shape:
-  //               OutlineInputBorder(borderRadius: BorderRadius.circular(1)),
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
