4c730f5ef8d399aca7bd06986f1efba9e41190a3
jh4843 <creamboy1@naver.com>
2024-02-18 22:19:14 +0900

    Firebase Storage

--- _7_firebase/lib/models/temp.dart
+++
@@ -1,9 +0,0 @@
-printInteger(int aNumber) {
-  print("The number is $aNumber.");
-}
-
-// starting point of the program
-main() {
-  var number = 42;
-  printInteger(number);
-}
--- _7_firebase/lib/views/memo_detail.dart
+++ _7_firebase/lib/views/memo_detail.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:_7_firebase/models/memo.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MemoDetailPage extends StatefulWidget {
   const MemoDetailPage(this.reference, this.memo, {Key? key}) : super(key: key);
@@ -20,6 +24,33 @@ class _MemoDetailPageState extends State<MemoDetailPage> {
     super.initState();
     titleController = TextEditingController(text: widget.memo.title);
     contentController = TextEditingController(text: widget.memo.content);
+  }
+
+  Future<void> saveMemoToFileAndUpload() async {
+    final dateTime = DateTime.now();
+    final fileName = '${dateTime.toIso8601String()}_memo.txt';
+    final localFile =
+        await _writeMemoToLocalFile(fileName, widget.memo.content);
+
+    try {
+      await FirebaseStorage.instance.ref('memos/$fileName').putFile(localFile);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Save memo as file and upload success!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error occured: $e')),
+      );
+    }
+  }
+
+  Future<File> _writeMemoToLocalFile(String fileName, String content) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/$fileName');
+
+    // Write the file (File Name and Content)
+    final fileContent = 'Title: $fileName\n\nContents: $content';
+    return file.writeAsString(fileContent);
   }
 
   @override
@@ -62,6 +93,12 @@ class _MemoDetailPageState extends State<MemoDetailPage> {
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          saveMemoToFileAndUpload();
+        },
+        child: const Icon(Icons.save_alt),
       ),
     );
   }
